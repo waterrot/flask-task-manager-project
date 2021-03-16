c8ffcd841d0fe8662433ad6c401742da7395f32b
waterrot <oudloosdrecht@gmail.com>
2021-03-16 23:56:50 +0000

    bind data to the edit task form

--- app.py
+++ app.py
@@ -123,6 +123,7 @@ def edit_task(task_id):
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_task.html", task=task, categories=categories)
 
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
